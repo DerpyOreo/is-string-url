8ead9907811ccfee45bc61e8fcfb5f25e01917f5
DerpyOreo <46456723+DerpyOreo@users.noreply.github.com>
2021-12-06 06:58:32 -0600

    yeah

---
+++ index.js
@@ -0,0 +1,13 @@
+function isStringUrl(string) {
+    let url
+
+    try {
+        url = new URL(string)
+    } catch (_) {
+        return false;
+    }
+
+    return url.protocol === "http:" || url.protocol === "https:"
+}
+
+module.exports = isStringUrl
